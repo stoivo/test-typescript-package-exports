8a0fef09952f8cfe75c94f47a5035b949eaac589
Simon <simon.toivo@telhaug.no>
2022-06-24 17:57:47 +0200

    init

---
+++ dep/src/main.ts
@@ -0,0 +1,4 @@
+import add from './add.js';
+import sub from './sub.js';
+
+export {add, sub}
---
+++ top/src/lol.ts
@@ -0,0 +1,10 @@
+import {add} from 'dep';
+import sub from 'dep/sub';
+
+export default function(){
+  console.log(`aaa`)
+  console.log(add(1,2))
+  console.log(sub(1,2))
+  console.log(`a a`)
+  console.log(`aaa`)
+}
